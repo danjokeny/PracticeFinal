3f91f20ef503fd99dba0a15dfa87c73a3c4d16cf
danjokeny <36682157+danjokeny@users.noreply.github.com>
2018-12-03 23:25:40 -0600

    tested some more

--- client/src/modules/foos.js
+++ client/src/modules/foos.js
@@ -1,17 +1,73 @@
-import {inject} from 'aurelia-framework';
-import {DataServices} from './data-services';
-@inject(DataServices)
+import { inject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { Foo } from '../resources/data/foos-object';
 
-export class foos {
-constructor(data) {
-        this.data = data;
-        this.USER_SERVICE = 'foos';
-    }
+
+@inject(Router, Foo)
+export class Foos {
+    constructor(router, foos) {
+        this.router = router;
+        this.foos = foos;
+        this.message = 'Foos';
+        this.showFooEditForm = false;
+    }
+
+    async activate() {
+        await this.getFoos();
+    }
+
+    attached() {
+        feather.replace()
+    }
+
+
+    async getFoos() {
+        await this.foos.getFoos();
+    }
+
+
+    newFoo() {
+        this.foo = {
+            foo: "",
+            woo: "",
+        }
+        this.openEditForm();
+    }
+
+    editFoo(foo) {
+        this.foo = foo;
+        this.openEditForm();
+    }
+
+    async delete() {
+        if (this.foo) {
+            await this.foos.delete(this.foo);
+            await this.getFoos();
+            this.back();
+        }
+    }
+
+
+    logout() {
+        this.router.navigate('home');
+    }
+
+
+    async save() {
+        if (this.foo && this.foo.foo && this.foo.woo) {
+            await this.foos.saveFoo(this.foo);
+            await this.getFoos();
+            this.back();
+        }
+    }
+
+
+    back() {
+        this.showFooEditForm = false;
+    }
+
+    openEditForm() {
+        this.showFooEditForm = true;
+        setTimeout(() => { $("#foo").focus(); }, 500);
+    }
 }
-
-export class Home {
-	constructor(){
-		this.message = "dannys foos table goes here";
-	}
-}
-
